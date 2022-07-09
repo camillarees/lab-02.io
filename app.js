3350a699c597e6db32f1fc0dd8d196b842a87167
Camilla Rees <camilla.c.rees@gmail.com>
2022-07-09 16:55:07 -0600

    added 4 yes or no questions and a welcome message

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+let user = prompt('What is your name?');
+console.log('Welcome ' + user)
 
 function getAnswer1 (){
     let guessFruit;
@@ -7,13 +9,13 @@ function getAnswer1 (){
         if (guessFruit != "yes") {
             alert("That's incorrect, please guess again")
         } else {
-            document.write("That's corret!");
+            document.write("That's correct!");
         }
 
     } while (guessFruit != "yes")
 }
 
-getAnswer2();
+getAnswer1();
 
 function getAnswer2 (){
     let guessColor;
@@ -22,12 +24,42 @@ function getAnswer2 (){
         if (guessColor != "no") {
             alert("That's incorrect, please guess again")
         } else {
-            document.write("That's corret!");
+            document.write("You're right. My favorite color is actually blue.");
         }
 
     } while (guessColor != "no")
 }
 
 getAnswer2();
+
+function getAnswer3 (){
+    let guessFlower;
+    do {
+        guessFlower = prompt("Is my favorite flower a rose?").toLowerCase();
+        if (guessFlower != "yes") {
+            alert("Sorry, you guessed wrong! Try again!")
+        } else {
+            document.write("Yeah it's not a rose. I actually love lilacs and peonies.");
+        }
+
+    } while (guessFlower != "yes")
+}
+
+getAnswer3();
+
+function getAnswer4 (){
+    let guessIcecream;
+    do {
+        guessIcecream = prompt("Is my favorite ice cream salted caramel?").toLowerCase();
+        if (guessIcecream != "yes") {
+            alert("Hm, that's not right. Try again buddy!")
+        } else {
+            document.write("YES! I love salted caramel");
+        }
+
+    } while (guessIcecream != "yes")
+}
+
+getAnswer4();
 
 
