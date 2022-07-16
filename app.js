8cbdf592ebd5e56db62ed3988a66bc8fe8496e18
Camilla Rees <camilla.c.rees@gmail.com>
2022-07-16 17:27:26 -0600

    added a question in js

--- app.js
+++ app.js
@@ -66,5 +66,64 @@
 
 // getAnswer4();
 
+// function getAnswer5 (){
+//     'use strict';
+//     let guessBeverage;
+//     do {
+//         guessBeverage = prompt("Is my favorite beverage a chai latte?").toLowerCase();
+//         if (guessBeverage != "yes") {
+//             alert("Nope! Try again.")
+//         } else {
+//             document.write("YES! I love chai!");
+//         }
 
+//     } while (guessBeverage != "yes")
+// }
 
+// getAnswer5();
+
+// function getAnswer6 (){
+// let correctAnswer = 14;
+// for (let i = 0; i < 4; i++) {
+//     console.log(i)
+//     let guessNumber = prompt("Guess a number");
+//     if (guessNumber == correctAnswer){
+//         alert("That's correct!"); 
+//         break;
+//       } else if (i >= 3) {
+//         alert("You've exhausted your attempts. The answer is 14.")
+//         break;
+//     } else if (guessNumber < correctAnswer) {
+//         alert("Too low, guess again.");
+//     } else if (guessNumber > correctAnswer) {
+//         alert("Too high, guess again.");
+//     } else { 
+//         alert("Not a valid input, try a number");
+//     }
+// }
+// }
+
+// getAnswer6();
+
+function getAnswer7 () {
+let whereLived = ['montana', 'washington', 'utah', 'arizona'];
+let guessState = prompt ("Can you guess which state I live in?").toLowerCase();
+let chances = 5
+
+while(whereLived.indexOf(whereLived) == -1) {
+    console.log(i);
+for (let i = 0; i < 7; i++)
+    if (guessState == whereLived) {
+        alert("That's correct!"); 
+        break;
+    } else if (guessState != correctAnswer2) {
+        alert("That's incorrect, try again.");
+    } else if (i >= 5) {
+        alert("You've exhausted your attempts. The answer is Utah.");
+        break;
+        }
+}
+}
+    getAnswer7();
+    
+// keep track of total number of correct answers and display how many out of 7 questions were correct
